b6afe8b912f9872bc116768480fc15bea44deeb3
Péter Kocsis <kopemi86@gmail.com>
2018-10-25 19:03:08 +0200

    feat: Component to make possible to add new deck to the collection

---
+++ components/AddDeck.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { View, TextInput, TouchableOpacity, Text} from 'react-native';
+import { addDeck } from './../actions/index';
+import { _addDeck } from './../utils/api';
+import styles from './../styles/index';
+
+class AddDeck extends Component {
+
+  state={
+    title:'',
+  }
+
+  onTitleChange=(input)=>{
+    this.setState(()=>({
+      title : input
+    }));
+  }
+
+  onSubmit=()=>{
+    const {decks, dispatch} = this.props;
+    debugger
+    const deckData = {
+        title : this.state.title,
+        questions : []
+    };
+    _addDeck(decks);
+    dispatch(addDeck(deckData))
+    this.setState(()=>({
+      title :''
+    }));
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text>What is the title of your new deck?</Text>
+        <TextInput value={this.state.title} onChangeText={this.onTitleChange}  placeholder='Title of the deck' ></TextInput>
+        <TouchableOpacity onPress={this.onSubmit}><Text>Create Deck</Text></TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+function mapStateToProps(decks){
+  debugger
+  return {
+    decks,
+  }
+}
+
+export default connect(mapStateToProps)(AddDeck);
