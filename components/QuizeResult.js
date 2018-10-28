57ccabdbdf73c24e560d1cd23d2d54d86d899b3e
Péter Kocsis <kopemi86@gmail.com>
2018-10-28 19:11:35 +0100

    feat: By dedicated buttons can navigate back to deck view or restart the quize

--- components/QuizeResult.js
+++ components/QuizeResult.js
@@ -2,16 +2,38 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './../styles/index';
+import { StackActions } from 'react-navigation';
 
 class QuizeResult extends Component {
+
+  onQuizeRestart=()=>{
+    const {deck} = this.props;
+    const replace = StackActions.replace({
+      routeName : 'Quize',
+      params : {
+        deckId: deck.title
+      }
+    });
+
+    this.props.navigation.dispatch(replace);
+  }
+
+  onBackToDeck=()=>{
+    const popAction = StackActions.pop({
+      n: 1,
+    });
+
+    this.props.navigation.dispatch(popAction);
+  }
+
   render(){
-    const {score, possibleMaxScore, navigation} = this.props;
+    const {score, possibleMaxScore} = this.props;
     return(
       <View style={styles.container}>
         <Text>Your score is:</Text>
         <Text>{`${score}/${possibleMaxScore}`}</Text>
-        <TouchableOpacity><Text>Restart Quize</Text></TouchableOpacity>
-        <TouchableOpacity><Text>Back to Deck</Text></TouchableOpacity>
+        <TouchableOpacity onPress={this.onQuizeRestart}><Text>Restart Quize</Text></TouchableOpacity>
+        <TouchableOpacity onPress={this.onBackToDeck}><Text>Back to Deck</Text></TouchableOpacity>
       </View>
     )
   }
