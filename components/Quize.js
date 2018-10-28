c2235485b698fd9a61968d62a0ae4c67df055816
Péter Kocsis <kopemi86@gmail.com>
2018-10-28 19:01:09 +0100

    fix: Back navigation should move to the individual deck view instead of the last state of the quize

--- components/Quize.js
+++ components/Quize.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './../styles/index';
+import { StackActions, NavigationActions } from 'react-navigation';
 
 class Quize extends Component {
 
@@ -17,68 +18,88 @@ class Quize extends Component {
     }));
   }
 
-  onCorrectAnswer = () => {
-    const {numberOfCards, navigation, deck} = this.props;
-    if(this.state.cardIndex < numberOfCards)
-    {
-      let currentIndex = this.state.cardIndex;
-      this.setState((previousState) => ({
-        cardIndex: previousState.cardIndex + 1,
-        showAnswer: false,
-        score: previousState.score + 1
-      }));
-      if(currentIndex +1 >= numberOfCards) {
-        navigation.navigate('QuizeResult', {deckId: deck.title, score: this.state.score + 1});
+  handleAnswer=(newState)=>{
+    const {deck, numberOfCards, navigation} = this.props;
+    Promise.resolve(
+      this.setState({
+        ...newState
+      })
+    )
+    .then(()=>{
+      const replaceAction = StackActions.replace({
+        routeName: 'QuizeResult',
+        params: {
+          deckId: deck.title,
+          score: this.state.score
+        }
+      })
+      if(this.state.cardIndex === numberOfCards) {
+        navigation.dispatch(replaceAction);
       }
-    }
+    });
+  }
+
+  onCorrectAnswer = () => {
+    const correAnswerState = {
+      cardIndex: this.state.cardIndex + 1,
+      showAnswer: false,
+      score: this.state.score + 1
+    };
+    this.handleAnswer(correAnswerState);
   }
 
   onIncorrectAnswer = () => {
-    const {numberOfCards, navigation, deck} = this.props;
-    if(this.state.cardIndex < numberOfCards)
-    {
-      let currentIndex = this.state.cardIndex;
-      this.setState((previousState) => ({
-        cardIndex: previousState.cardIndex + 1,
-        showAnswer: false,
-      }));
-      if(currentIndex +1 >= numberOfCards) {
-        navigation.navigate('QuizeResult', {deckId: deck.title, score: this.state.score});
-      }
-    }
+    const incorrectAnswerState = {
+      cardIndex: this.state.cardIndex + 1,
+      showAnswer: false,
+      score: this.state.score
+    };
+    this.handleAnswer(incorrectAnswerState);
   }
 
   render() {
     const { deck, numberOfCards } = this.props;
-    const questionCard = deck.questions[this.state.cardIndex];
-    const cardUnavailable = questionCard ? false : true;
-    debugger
-    return (
-      <View style={styles.container}>
-        {cardUnavailable
-          ? <Text>Can not start quize, there is no card in the deck</Text>
-          : (
-            <View>
-              <Text>{`${this.state.cardIndex + 1}/${numberOfCards}`}</Text>
-              {this.state.showAnswer
-                ? (
-                  <View>
-                    <Text>{questionCard.answer}</Text>
-                    <TouchableOpacity onPress={this.onCardFlip}><Text>Show Question</Text></TouchableOpacity>
-                  </View>
-                )
-                : (
-                  <View>
-                    <Text>{questionCard.question}</Text>
-                    <TouchableOpacity onPress={this.onCardFlip}><Text>Show Answer</Text></TouchableOpacity>
-                  </View>
-                )
-              }
-              <TouchableOpacity onPress={this.onCorrectAnswer}><Text>Correct</Text></TouchableOpacity>
-              <TouchableOpacity onPress={this.onIncorrectAnswer}><Text>Incorrect</Text></TouchableOpacity>
-            </View>)}
-      </View>
-    )
+
+    if(numberOfCards === 0) {
+      return (
+        <View style={styles.container}>
+          <Text>Can not start quize, there is no card in the deck</Text>
+        </View>
+      )
+    }
+
+    if(numberOfCards === this.state.cardIndex)
+    {
+      //This is a blank view which will be show only for a friction until the QuizeResult view will be visible
+      return (
+        <View style={styles.container}>
+        </View>
+      )
+    }
+    else{
+      const questionCard = deck.questions[this.state.cardIndex];
+      return (
+        <View style={styles.container}>
+          <Text>{`${this.state.cardIndex + 1}/${numberOfCards}`}</Text>
+          {this.state.showAnswer
+            ? (
+              <View>
+                <Text>{questionCard.answer}</Text>
+                <TouchableOpacity onPress={this.onCardFlip}><Text>Show Question</Text></TouchableOpacity>
+              </View>
+            )
+            : (
+              <View>
+                <Text>{questionCard.question}</Text>
+                <TouchableOpacity onPress={this.onCardFlip}><Text>Show Answer</Text></TouchableOpacity>
+              </View>
+            )
+          }
+          <TouchableOpacity onPress={this.onCorrectAnswer}><Text>Correct</Text></TouchableOpacity>
+          <TouchableOpacity onPress={this.onIncorrectAnswer}><Text>Incorrect</Text></TouchableOpacity>
+        </View>
+      )
+    }
   }
 }
 
