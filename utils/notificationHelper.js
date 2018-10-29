dde34e3f43e451a10293fba852683d1efdaa71ce
Péter Kocsis <kopemi86@gmail.com>
2018-10-29 08:26:23 +0100

    fix: Set notification time to a more useful time

--- utils/notificationHelper.js
+++ utils/notificationHelper.js
@@ -40,8 +40,8 @@ export function setLocalNotification () {
 
               let tomorrow = new Date()
               tomorrow.setDate(tomorrow.getDate() + 1)
-              tomorrow.setHours(20)
-              tomorrow.setMinutes(8)
+              tomorrow.setHours(15)
+              tomorrow.setMinutes(0)
 
               Notifications.scheduleLocalNotificationAsync(
                 createNotification(),
