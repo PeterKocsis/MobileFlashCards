d97f0b0db6c9a77989194d94af40b0bdfe7ebd90
Péter Kocsis <kopemi86@gmail.com>
2018-10-30 19:46:25 +0100

    fix: Make animation only on the selected item instead of all item

---
+++ components/Deck.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Text, TouchableOpacity, Animated } from 'react-native';
+import styles from './../styles/index';
+
+class Deck extends Component {
+  state={
+    bounceAnim : new Animated.Value(1),
+  }
+
+  onDeckSelection=(deck)=>{
+    const {navigation} = this.props;
+    Animated.sequence([
+      Animated.timing(this.state.bounceAnim, { duration: 200, toValue: 1.04}),
+      Animated.spring(this.state.bounceAnim, { toValue: 1, friction: 4})
+    ]).start(()=>navigation.navigate('DeckView', {deckId:deck.title}))
+  }
+
+  render(){
+    const {deck} = this.props;
+    debugger
+    return(
+      <Animated.View style={{transform: [{scale: this.state.bounceAnim}]}}>
+        <TouchableOpacity
+          onPress={()=>this.onDeckSelection(deck)}
+          style={styles.card}>
+          <Text style={styles.mainText}>{deck.title}</Text>
+          <Text style={styles.text}>{`Cards: ${deck.questions.length}`}</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    )
+  }
+}
+
+function mapStateToProps(decks, {nav, deckItem}){
+  debugger
+  return {
+    navigation: nav,
+    deck: deckItem
+  };
+}
+
+export default connect(mapStateToProps)(Deck);
--- components/DeckItem.js
+++ components/DeckItem.js
@@ -1,39 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text, TouchableOpacity, FlatList, Animated } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import styles from './../styles/index';
 import { handleReceiveDecks } from './../actions/index';
+import Deck from './Deck';
 
 class DeckItem extends Component {
-
-  state={
-    bounceAnim : new Animated.Value(1),
-  }
 
   componentDidMount() {
     this.props.dispatch(handleReceiveDecks());
   }
 
-  onDeckSelection=(item)=>{
-    const {navigation} = this.props;
-    Animated.sequence([
-      Animated.timing(this.state.bounceAnim, { duration: 200, toValue: 1.04}),
-      Animated.spring(this.state.bounceAnim, { toValue: 1, friction: 4})
-    ]).start(()=>navigation.navigate('DeckView', {deckId:item}))
-  }
-
   renderItem=({item})=>{
     const {decks, navigation} = this.props;
     const deck = decks[item];
+    debugger
     return (
-      <Animated.View style={{transform: [{scale: this.state.bounceAnim}]}}>
-        <TouchableOpacity
-          onPress={()=>this.onDeckSelection(item)}
-          style={styles.card}>
-          <Text style={styles.mainText}>{deck.title}</Text>
-          <Text style={styles.text}>{`Cards: ${deck.questions.length}`}</Text>
-        </TouchableOpacity>
-      </Animated.View>
+      <Deck deckItem={deck} nav={navigation}></Deck>
     );
   }
 
@@ -48,7 +31,7 @@ class DeckItem extends Component {
     debugger
     return(
       <View style={styles.container}>
-        {noDeckinDecks ? (<Text style={styles.title}>No deck has been created. You can create your own deck on the "Add Deck" tab.</Text>)
+        {noDeckinDecks ? (<Text style={styles.title}>You can create your own deck on the "Create Deck" tab.</Text>)
         :<FlatList
           data = {Object.keys(decks)}
           renderItem={this.renderItem}
